c8055be84ba7266734dd04ecf6847423dece3c73
Silviu Ojog <sil.ojog@gmail.com>
2024-11-07 15:45:31 +0200

    Pandas

---
+++ Ziua2/Pandas/clwin.py
@@ -0,0 +1,15 @@
+# 1. Importați fișierul CSV folosind Pandas și afișați primele 5 rânduri.
+# 2. Afișați tipurile de date ale fiecărei coloane.
+# 3. Obțineți statisticile rezumate ale coloanelor numerice din date.
+# 4. Verificați dacă există valori lipsă în setul de date.
+# 5. Redenumiți coloana „echipă” în „club”.
+# 6. Selectați și afișați coloanele „echipă” și „trofee”.
+# 7. Selectați rândurile în care țara este „Anglia”.
+# 8. Afișează rândurile în care „trofeele” sunt mai mari decât 2.
+# 9. Accesați coloanele „echipă” și „țară” pentru primele 10 rânduri.
+# 10. Selectați ultimele 5 rânduri ale DataFra
+# me.
+# 11. Filtrați echipele din „Spania” și afișați-le datele.
+# 12. Găsiți toate echipele care au câștigat exact 2 trofee.
+# 13. Filtrați echipele în care țara este fie „Anglia”, fie „Italia”.
+# 14. Selectează echipele care au mai mult de 1 dar mai puțin de 3 trofee.
